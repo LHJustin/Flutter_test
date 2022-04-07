d9c301f1272a73cdac7a8d960bad636e8180dd76
justin_lin <masterofleaf@gmail.com>
2022-04-07 18:01:42 +0800

    eighth commmit searchpage layout is broken

--- lib/home.dart
+++ lib/home.dart
@@ -38,13 +38,13 @@ class _BodyState extends State<Body> {
     Response response = await dio.get('https://api.jsonserve.com/qHsaqy');
     print('${response.data['result']['lightyear_list'].length}');
     List<dynamic> jlist = response.data['result']['lightyear_list'];
-    jlist.forEach(( element) {
-       Map<String, dynamic> map = element;
+    jlist.forEach((element) {
+      Map<String, dynamic> map = element;
       print('$map');
     });
     List<LightyearList> programlist = [];
-    for(Map<String, dynamic> i in jlist){
-      programlist.add (LightyearList.fromJson(i));
+    for (Map<String, dynamic> i in jlist) {
+      programlist.add(LightyearList.fromJson(i));
     }
     // var programList = response.data['result']['lightyear_list'].map((m) => new LightyearList.fromJson(m)).toList();
     return programlist;
@@ -52,58 +52,63 @@ class _BodyState extends State<Body> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(5),
-      mainAxisSpacing: 5,
-      crossAxisSpacing: 5,
-      crossAxisCount: 2,
-      children: <Widget>[
-        FutureBuilder(
-          future: getProgram(),
-          builder: (BuildContext context, AsyncSnapshot<List<LightyearList>> snapshot){
-            if (!snapshot.hasData) {
-              print("no data");
-              return Container();
-            }
-            print(snapshot.data?[0].streamId);
-            return Container(
-              color: Colors.purple,
-              child: GestureDetector(
+    return FutureBuilder(
+      future: getProgram(),
+      builder: (BuildContext context, AsyncSnapshot<List<LightyearList>> snapshot) {
+        if (!snapshot.hasData) {
+          print("no data");
+          return Container();
+        }
+        print(snapshot.data?[0].streamId);
+        return GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(5),
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          crossAxisCount: 2,
+          children: <Widget>[
+            for (int i = 0; i < snapshot.data!.length; i++)
+              GestureDetector(
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) => chatroom()));
                 },
-                child: Stack(
-                  children: [
-                    FadeInImage.assetNetwork(placeholder: "Images/mm.jpg", image: "https://storage.googleapis.com/lottcube/production/1/headphoto/headphoto1644397354.png"),
-                    Column(
-                      children: const [
-                        Text(
-                          "data",
-                          style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 30),
-                        )
-                      ],
-                    ),
-                    Column(
-                      verticalDirection: VerticalDirection.up,
-                      children: [
-                        Text(
-                          "cc",
-                          style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 30),
-                        ),
-                        Text(
-                          "data",
-                          style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white),
-                        ),
-                      ],
-                    )
-                  ],
+                child: Container(
+                  color: Colors.purple,
+                  child: Stack(
+                    children: [
+                      FadeInImage.assetNetwork(
+                        placeholder: "Images/mm.jpg",
+                        image: "${snapshot.data![i].headPhoto}",
+                        fit: BoxFit.cover,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '${snapshot.data![i].streamTitle}',
+                            style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 20),
+                          )
+                        ],
+                      ),
+                      Column(
+                        verticalDirection: VerticalDirection.up,
+                        children: [
+                          Text(
+                            "${snapshot.data![i].nickname}",
+                            style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 24),
+                          ),
+                          Text(
+                            "${snapshot.data![i].tags}",
+                            style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-            );
-          },
-        )
-      ],
+          ],
+        );
+      },
     );
   }
 }
--- lib/search.dart
+++ lib/search.dart
@@ -1,8 +1,9 @@
-
-
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'chatroom.dart';
+import 'json/jsonprogram.dart';
 import 'main.dart';
 
 class search extends StatelessWidget {
@@ -11,21 +12,109 @@ class search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ElevatedButton(
-        child: Text('返回首頁'),
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
-        },
-      ),
+      body: Body(),
     );
   }
 }
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+  List jlist = [];
+
+  Future<List<LightyearList>> getProgram() async {
+    var dio = Dio();
+    Response response = await dio.get('https://api.jsonserve.com/qHsaqy');
+    print('${response.data['result']['lightyear_list'].length}');
+    List<dynamic> jlist = response.data['result']['lightyear_list'];
+    jlist.forEach((element) {
+      Map<String, dynamic> map = element;
+      print('$map');
+    });
+    List<LightyearList> programlist = [];
+    for (Map<String, dynamic> i in jlist) {
+      programlist.add(LightyearList.fromJson(i));
+    }
+    // var programList = response.data['result']['lightyear_list'].map((m) => new LightyearList.fromJson(m)).toList();
+    return programlist;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: getProgram(),
+      builder: (BuildContext context, AsyncSnapshot<List<LightyearList>> snapshot) {
+        if (!snapshot.hasData) {
+          print("no data");
+          return Container();
+        }
+        print(snapshot.data?[0].streamId);
+        return SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(),
+                Text("data"),
+                GridView.count(
+                  crossAxisCount: 2,
+                  primary: false,
+                  padding: const EdgeInsets.all(5),
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  children: <Widget>[
+                    Container(),
+                  ],
+                ),
+                Text("data"),
+                GridView.count(
+                  primary: false,
+                  padding: const EdgeInsets.all(5),
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    for (int i = 0; i < snapshot.data!.length; i++)
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => chatroom()));
+                        },
+                        child: Container(
+                          color: Colors.purple,
+                          child: Stack(
+                            children: [
+                              FadeInImage.assetNetwork(
+                                placeholder: "Images/mm.jpg",
+                                image: "${snapshot.data![i].headPhoto}",
+                                fit: BoxFit.cover,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    '${snapshot.data![i].streamTitle}',
+                                    style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 20),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                verticalDirection: VerticalDirection.up,
+                                children: [
+                                  Text(
+                                    "${snapshot.data![i].nickname}",
+                                    style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white, fontSize: 24),
+                                  ),
+                                  Text(
+                                    "${snapshot.data![i].tags}",
+                                    style: TextStyle(backgroundColor: Color.fromARGB(100, 256, 256, 256), color: Colors.white),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+        );
+      },
+    );
   }
 }
